8e6dbaad2dc8285655e7e696b4aeb0a2f8206b4a
Crogen0407 <mymcoding0407@gmail.com>
2024-01-12 00:38:46 +0900

    feat : SkillController

---
+++ Assets/01.Scripts/SO/SO_SkillData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Crogen.BishojyoGraph
+{
+    [CreateAssetMenu(fileName = "SkillData", menuName = "SO/SkillData", order = 0)]
+    public class SO_SkillData : ScriptableObject
+    {
+        
+    }
+}
