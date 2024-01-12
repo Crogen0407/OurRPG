1a81abee74ae55d4d65fd3dc8a12529c607284a2
Crogen0407 <mymcoding0407@gmail.com>
2024-01-13 00:46:57 +0900

    feat : SkillComplete

---
+++ Assets/01.Scripts/Skill/GeneralMagicball.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GeneralMagicball : Skill
+{
+    [SerializeField] private GameObject _skillEffect;
+    [SerializeField] private float _speed = 15;
+
+    public override void Init()
+    {
+        GameObject obj = Instantiate(_skillEffect, PlayerMovement.PlayerPosition, Quaternion.identity);
+        obj.transform.up = PlayerAttackDirection.Direction;
+        obj.GetComponent<Rigidbody2D>().velocity = obj.transform.up * _speed;
+    }
+}
